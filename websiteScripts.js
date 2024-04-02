469ced6f5f8daf9b23c4f941fb35e5c8184c45a2
Larry <44798880+Larry@users.noreply.github.com>
2024-04-02 18:59:15 +0200

    add temporary content hiding and visibility feature, this feature needs to be refactored

--- websiteScripts.js
+++ websiteScripts.js
@@ -1,2 +1,24 @@
-const myHeading = document.querySelector("h1");
-myHeading.textContent = "Hello world!";
+
+function DummyProjectOneExpandingContent() {
+    document.getElementById("DummyProjectOneContentNotExpanded").classList.toggle("DummyProjectOneContentExpanded");
+}
+
+function DummyProjectTwoExpandingContent() {
+    document.getElementById("DummyProjectTwoContentNotExpanded").classList.toggle("DummyProjectTwoContentExpanded");
+}
+
+function AusbildungExpandingContent() {
+    document.getElementById("AusbildungContentNotExpanded").classList.toggle("AusbildungContentExpanded");
+}
+
+function BerufserfahrungExpandingContent() {
+    document.getElementById("BerufserfahrungContentNotExpanded").classList.toggle("BerufserfahrungContentExpanded");
+}
+
+function SkillsExpandingContent() {
+    document.getElementById("SkillsContentNotExpanded").classList.toggle("SkillsContentExpanded");
+}
+
+function HobbiesExpandingContent() {
+    document.getElementById("HobbiesContentNotExpanded").classList.toggle("HobbiesContentExpanded");
+}
