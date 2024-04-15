fc8674e1fe5d704a83079334179beedfa74bce4d
Larry <44798880+Larry@users.noreply.github.com>
2024-04-15 20:30:03 +0200

    add a function which loops through a list of queried classes called >>calcualtorNumber<< and splits of the last string of each unique id that has the class of >>calcualtorNumber<<. The split of string happens to be an integer

--- websiteScripts.js
+++ websiteScripts.js
@@ -1,9 +1,17 @@
+/* searchin for all classes that have the class calulatorNumber */
 const numbers = document.querySelectorAll(".calculatorNumber");
 
+/* Looping through the amount of classes found with the querySelectorAll method and then printing out the 6th string when clicking on the button*/
 for(var i=0; i<numbers.length; i++) {
-  numbers[i].addEventListener("click", showNumbers);
+  numbers[i].addEventListener("click", showNumber);
 }
 
-function showNumbers(e){
- console.log(e.target.id);
+
+function showNumber(e){
+    /* set the id's of the pressed buttons to the const stringID */
+    const stringID = e.target.id;
+
+    /* Splitting of the last strings of the id's number1, to number9, so that the id number1 becomes only the string 1*/
+    const splitStringID = stringID.split("");
+    console.log(splitStringID[6]);
 }
