a0044d3ab5930e99b4d3f24cf1eb40afd7a3bfd6
Larry <44798880+Larry@users.noreply.github.com>
2024-04-17 19:10:52 +0200

    changed all ids and classes from the word number to the word character

--- websiteScripts.js
+++ websiteScripts.js
@@ -1,17 +1,17 @@
 /* searchin for all classes that have the class calulatorNumber */
-const numbers = document.querySelectorAll(".calculatorNumber");
+const numbers = document.querySelectorAll(".calculatorCharacter");
 
 /* Looping through the amount of classes found with the querySelectorAll method and then printing out the 6th string when clicking on the button*/
 for(var i=0; i<numbers.length; i++) {
-  numbers[i].addEventListener("click", showNumber);
+  numbers[i].addEventListener("click", showCharacter);
 }
 
 
-function showNumber(uniqueIDButton){
+function showCharacter(uniqueIDButton){
     /* set the id's of the pressed buttons to the const stringID */
     const stringID = uniqueIDButton.target.id;
 
     /* Splitting of the last strings of the id's number1, to number9, so that the id number1 becomes only the string 1*/
     const splitStringID = stringID.split("");
-    console.log(splitStringID[6]);
+    console.log(splitStringID[9]);
 }
