ec41f803a8b71370927b24d23398b552f99381af
Larry <44798880+Larry@users.noreply.github.com>
2024-04-17 20:20:26 +0200

    replaced function >>showCharacter<< with function >>inputCharacter<< which now take the value of each button instead of splitting the end of each buttons id's string. Also added a value to each button.

--- websiteScripts.js
+++ websiteScripts.js
@@ -3,16 +3,26 @@ const characters = document.querySelectorAll(".calculatorCharacter");
 
 /* Looping through the amount of classes found with the querySelectorAll method and then printing out the 6th string when clicking on the button*/
 for(var i=0; i<characters.length; i++) {
-  characters[i].addEventListener("click", showCharacter);
+  characters[i].addEventListener("click", inputCharacter);
 }
 
-function showCharacter(uniqueIDButton){
-    /* set the id's of the pressed buttons to the const stringID */
-    const stringID = uniqueIDButton.target.id;
+//####################### OLD FUNCTION########################################
+// function showCharacter(uniqueIDButton){
+//     /* set the id's of the pressed buttons to the const stringID */
+//     const stringID = uniqueIDButton.target.id;
 
-    /* Splitting of the last strings of the id's number1, to number9, so that the id number1 becomes only the string 1*/
-    const splitStringID = stringID.split("");
-    
-    var x = document.getElementById("calculatorInputField");
-    x.setAttribute(localName, splitStringID[9]);
+//     /* Splitting of the last strings of the id's number1, to number9, so that the id number1 becomes only the string 1*/
+//     const splitStringID = stringID.split("");
+// }
+
+function inputCharacter(buttonValueParamter){
+  // If the event does not get explicitly handled, its default action should not be taken as it normally would be --MDN
+  buttonValueParamter.preventDefault();
+
+  // looking up all the looped buttons values and setting it to the const buttonValue
+  const buttonValue = buttonValueParamter.target.value;
+
+  // input the looped buttons values into the text field
+  document.getElementById("calculatorInputField").value = buttonValue;
+  console.log(buttonValue);
 }
