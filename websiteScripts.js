bc617b64ad6a61a58b180433f4e0da84162750c6
Larry <44798880+Larry@users.noreply.github.com>
2024-04-17 19:46:10 +0200

    changed the name from numbers to chracters in the queryselector and the for loop

--- websiteScripts.js
+++ websiteScripts.js
@@ -1,11 +1,10 @@
 /* searchin for all classes that have the class calulatorNumber */
-const numbers = document.querySelectorAll(".calculatorCharacter");
+const characters = document.querySelectorAll(".calculatorCharacter");
 
 /* Looping through the amount of classes found with the querySelectorAll method and then printing out the 6th string when clicking on the button*/
-for(var i=0; i<numbers.length; i++) {
-  numbers[i].addEventListener("click", showCharacter);
+for(var i=0; i<characters.length; i++) {
+  characters[i].addEventListener("click", showCharacter);
 }
-
 
 function showCharacter(uniqueIDButton){
     /* set the id's of the pressed buttons to the const stringID */
@@ -13,5 +12,7 @@ function showCharacter(uniqueIDButton){
 
     /* Splitting of the last strings of the id's number1, to number9, so that the id number1 becomes only the string 1*/
     const splitStringID = stringID.split("");
-    console.log(splitStringID[9]);
+    
+    var x = document.getElementById("calculatorInputField");
+    x.setAttribute(localName, splitStringID[9]);
 }
